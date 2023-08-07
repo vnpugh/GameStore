32f04bf660e3e2c11a5cd33e7ee3bb3531c8cffd
vnpugh <vnp19@yahoo.com>
2023-08-07 00:15:08 -0500

    updating user and profile models

--- src/main/java/com/gamestore/model/User.java
+++ src/main/java/com/gamestore/model/User.java
@@ -1,4 +1,100 @@
 package com.gamestore.model;
 
-public class User {
-}
+import jakarta.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+    @Entity
+    @Table(name = "users")
+    public class User {
+        @Id
+        @Column
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column
+        private String firstName;
+        @Column
+        private String lastName;
+
+        @Column(unique = true)
+        private String email;
+
+        @Column
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        private String password;
+
+        @OneToOne(cascade = CascadeType.ALL)
+        @JoinColumn(name = "profile_id", referencedColumnName = "id")
+        private UserProfile userProfile;
+
+        public User() {
+        }
+
+        public User(String firstName, String lastName, String email, String password) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public UserProfile getUserProfile() {
+            return userProfile;
+        }
+
+        public void setUserProfile(UserProfile userProfile) {
+            this.userProfile = userProfile;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", email='" + email + '\'' +
+                    ", password='" + password + '\'' +
+                    ", userProfile=" + userProfile +
+                    '}';
+        }
+    }
--- src/main/java/com/gamestore/model/UserProfile.java
+++ src/main/java/com/gamestore/model/UserProfile.java
@@ -1,4 +1,102 @@
 package com.gamestore.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_profiles")
 public class UserProfile {
+        @Id
+        @Column
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+    @Column
+    private String shippingAddress;
+
+    @Column
+    private String billingAddress;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+        /**
+         * One-To-One UserProfile to User. Every user gets a user profile that holds information.
+         */
+        @JsonIgnore
+        @OneToOne(mappedBy = "userProfile")
+        private User user;
+
+        public UserProfile() {
+        }
+
+        public UserProfile(String shippingAddress, String billingAddress, String phoneNumber, String email) {
+            this.shippingAddress = shippingAddress;
+            this.billingAddress = billingAddress;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id=" + id +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                ", billingAddress='" + billingAddress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
