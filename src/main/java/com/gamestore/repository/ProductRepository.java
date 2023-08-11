cb4cd8392adea6ba6342b0231e64ad3bc93c3621
vnpugh <vnp19@yahoo.com>
2023-08-11 15:44:50 -0500

    Added Product Repository

---
+++ src/main/java/com/gamestore/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.gamestore.repository;
+
+
+import com.gamestore.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductName(String productName);
+
+}
