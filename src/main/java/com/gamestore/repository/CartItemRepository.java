6be2885a047cd47c94b0071132a43617c9929c62
vnpugh <vnp19@yahoo.com>
2023-08-11 16:09:39 -0500

    Added Category Repository

--- src/main/java/com/gamestore/repository/CartItemRepository.java
+++ src/main/java/com/gamestore/repository/CartItemRepository.java
@@ -2,9 +2,11 @@ package com.gamestore.repository;
 
 import com.gamestore.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findByCartId(Long cartId);
--- src/main/java/com/gamestore/repository/CheckoutRepository.java
+++ src/main/java/com/gamestore/repository/CheckoutRepository.java
@@ -2,6 +2,8 @@ package com.gamestore.repository;
 
 import com.gamestore.model.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 }
