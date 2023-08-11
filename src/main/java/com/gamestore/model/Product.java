acdd7bf5778678d7bf9df6030d232ed8fb82ed91
vnpugh <vnp19@yahoo.com>
2023-08-11 13:27:34 -0500

    Updated Product Model

--- src/main/java/com/gamestore/model/Product.java
+++ src/main/java/com/gamestore/model/Product.java
@@ -1,9 +1,11 @@
 package com.gamestore.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "products")
@@ -30,92 +32,33 @@ public class Product {
 
     private LocalDate dateProductAdded;
 
+    @Column
+    private String productImage;
+
     //A product can only belong to ONE category.
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
 
 
+    @OneToMany(mappedBy = "productName")
+    @JsonIgnore
+    private List<CartItem> cartItems;
+
     public Product() {
     }
 
     public Product(Long id, String productName, String productDescription, Double productPrice,
-                   int productQuantity, LocalDate dateProductAdded, Category category) {
+                   int productQuantity, LocalDate dateProductAdded, String productImage,
+                   Category category, List<CartItem> cartItems) {
         this.id = id;
         this.productName = productName;
         this.productDescription = productDescription;
         this.productPrice = productPrice;
         this.productQuantity = productQuantity;
         this.dateProductAdded = dateProductAdded;
+        this.productImage = productImage;
         this.category = category;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public Double getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(Double productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public int getProductQuantity() {
-        return productQuantity;
-    }
-
-    public void setProductQuantity(int productQuantity) {
-        this.productQuantity = productQuantity;
-    }
-
-    public LocalDate getDateProductAdded() {
-        return dateProductAdded;
-    }
-
-    public void setDateProductAdded(LocalDate dateProductAdded) {
-        this.dateProductAdded = dateProductAdded;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", productName='" + productName + '\'' +
-                ", productDescription='" + productDescription + '\'' +
-                ", productPrice=" + productPrice +
-                ", productQuantity=" + productQuantity +
-                ", dateProductAdded=" + dateProductAdded +
-                ", category=" + category +
-                '}';
+        this.cartItems = cartItems;
     }
 }
