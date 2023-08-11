081d6dfc23a014363a23443a1f8c5340010002a4
vnpugh <vnp19@yahoo.com>
2023-08-10 22:20:47 -0500

    Added Category

--- src/main/java/com/gamestore/model/Category.java
+++ src/main/java/com/gamestore/model/Category.java
@@ -1,4 +1,54 @@
 package com.gamestore.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String categoryName;
+    private String categoryDescription;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                ", categoryDescription='" + categoryDescription + '\'' +
+                '}';
+    }
 }
